1bb962715434c50aa2fe5b1d231169b5035af9a6
saurabh-uchil <uchilsaurabh@gmail.com>
2022-01-24 02:01:24 +1100

    Average Order Value done

--- average-order-value/test_data.sql
+++ average-order-value/test_data.sql
@@ -13,3 +13,7 @@ insert into orders (order_id,customer_id,ordered_at) values (10248,3,'1996-07-04
 insert into products (product_id,name) values (11,'Rambutan'),(42,'The Matrix Reloaded'),(72,'Milk (2 litres)'),(14,'QZ-183'),(51,'Oak Plank (2m x 4m)'),(31,'Loot Crate'),(39,'F-111 Aardvark'),(49,'Fresh Air'),(71,'Bowler Hat'),(24,'12 month subscription'),(59,'Bunch of Roses') ;
 
 insert into order_line_items (order_line_item_id,order_id,product_id,unit_price,quantity) values (24601,10248,11,14,12),(24602,10248,42,9,10),(24603,10248,72,34,5),(24604,10249,14,18,9),(24605,10249,51,42,40),(24606,10253,31,10,20),(24607,10253,39,14,42),(24608,10253,49,16,40),(24609,10274,71,17,20),(24610,10274,72,27,7),(24611,10275,24,3,12),(24612,10275,59,44,6),(24613,10296,11,16,12),(24614,10296,51,13,30),(24615,10296,39,28,15),(24616,10275,72,3,3),(24617,10275,39,50,4),(24618,10275,11,4,5) ;
+
+
+<!-- Answer -->
+select name, count(a.order_id) as num_orders, sum(total) as total_order_value, round(sum(total)/count(a.order_id)) as avg_order_value from (select customers.name, orders.order_id from orders inner join customers on orders.customer_id = customers.customer_id) a full join (select orders.order_id , orders.customer_id, sum(order_line_items.unit_price * order_line_items.quantity) as total from orders inner join order_line_items on orders.order_id = order_line_items.order_id group by orders.order_id) b on a.order_id = b.order_id group by name order by avg_order_value desc;
